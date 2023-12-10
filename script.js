35aa85f776053631924c9c00336335ff176f02db
Trophoston <95759773+Trophoston@users.noreply.github.com>
2023-12-11 02:32:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const inp = document.getElementById("searchinp");
+const but = document.getElementById("searchbut");
+
+but.addEventListener("click", function() {
+  command(inp);
+});
+
+function command(a) {
+  if (a.value === "h") {
+    
+  } else if (a.value === "c") {
+    
+  } else {
+    
+  }
+}
